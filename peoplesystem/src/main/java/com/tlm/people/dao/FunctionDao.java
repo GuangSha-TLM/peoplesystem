38d5cd3eac2b7b469ca19d4bb6ee035286c0c9df
Jasper Han <18945342298@163.com>
2024-03-05 19:20:40 +0800

    2024-3-5-19:20 hln （文件下载-无bug）

--- peoplesystem/src/main/java/com/tlm/people/dao/FunctionDao.java
+++ peoplesystem/src/main/java/com/tlm/people/dao/FunctionDao.java
@@ -1,7 +1,13 @@
 package com.tlm.people.dao;
 
+import com.tlm.people.entity.User;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface FunctionDao {
+
+    //查询所有
+    List<User> findAll();
 }
--- peoplesystem/src/main/java/com/tlm/people/service/impl/FunctionServiceImpl.java
+++ peoplesystem/src/main/java/com/tlm/people/service/impl/FunctionServiceImpl.java
@@ -2,15 +2,19 @@ package com.tlm.people.service.impl;
 
 import com.alibaba.excel.EasyExcel;
 import com.tlm.people.dao.FunctionDao;
+import com.tlm.people.entity.User;
 import com.tlm.people.entity.vo.FunctionExcelVo;
 import com.tlm.people.service.FunctionService;
 import com.tlm.people.utils.ExcelListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class FunctionServiceImpl implements FunctionService {
@@ -46,8 +50,21 @@ public class FunctionServiceImpl implements FunctionService {
             //设置响应头
             response.setHeader("Content-disposition","filename=" + fileName + ".xlsx");
 
-            //
+            //查询所有分类，返回list集合
+            List<User> userList = functionDao.findAll();
 
+            //最终数据list集合
+            List<FunctionExcelVo> functionExcelVoList = new ArrayList<>();
+
+            for (User user : userList) {
+                FunctionExcelVo functionExcelVo = new FunctionExcelVo();
+                BeanUtils.copyProperties(user,functionExcelVo);
+                functionExcelVoList.add(functionExcelVo);
+            }
+
+            //写入操作
+            EasyExcel.write(response.getOutputStream(),FunctionExcelVo.class)
+                    .sheet("数据").doWrite(functionExcelVoList);
         } catch (Exception e){
             e.printStackTrace();
             throw new RuntimeException(e);
